228f8b4ea8d97599dfc0a4d92708fab79d4290ca
Jeremie <jeremie.de-pas@epitech.eu>
2018-03-08 17:43:35 +0100

    modification bot

--- enigme_3/bot.py
+++ enigme_3/bot.py
@@ -3,17 +3,17 @@ import requests
 import os
 from time import sleep
 from pprint import pprint
-
+server = "http://localhost:5000/"
 os.system("clear")
 print("\n")
 with open('./question_reponse.json', 'r') as json_data:
     histoire = json.load(json_data)
 v = 0
 while v != "1":
-    r = requests.get('http://localhost:5000/enigme_bot')
+    r = requests.get(server+'enigme_bot')
     v = r.text
     sleep(0.5)
-os.system('cvlc --no-audio --play-and-exit -V caca ascii-video-kingsmen480p.mov')
+# os.system('cvlc --no-audio --play-and-exit -V caca ascii-video-kingsmen1080p2.mp4')
 print(histoire['partie_1']['introduction']+"\n")
 partie = "partie_1"
 
@@ -23,6 +23,12 @@ while True:
         if var in histoire[partie]['attendu']:
             x = histoire['partie_1']["reponse"][var]
             print("#>"+ histoire[x]["reponse"][var])
+        if var == "404":
+            r = requests.post(server+'enigme_bot/bot/1', data = {'porte': '2', "etat": "1"})
+            print("#> "+ r.text)
+        if var == "9467":
+            r = requests.post(server+'enigme_bot/bot/1', data = {'porte': '3', "etat": '1'})
+            print("#> oki")
         if var == "exit":
             exit()
     except KeyboardInterrupt:
