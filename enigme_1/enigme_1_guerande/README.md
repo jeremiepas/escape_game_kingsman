443f54aee2c73b7986f2d7a9e0fc93f774a249f5
Jeremie <jeremie.de-pas@epitech.eu>
2018-03-25 22:14:03 +0200

    creation readme.md enigme 1

--- enigme_1/enigme_1_guerande/README.md
+++ enigme_1/enigme_1_guerande/README.md
@@ -1,14 +1,46 @@
 # escape game guerande
 ## enigme 1 porte menteau
 
-prestion diferente sur 4 porte menteau ouvre une trape cache.
+pression différente sur 4 porte manteau ouvre une trappe.
 
+### branchement
+* branchement capteur
 
-A0 = porte menteaux un ecoute 
-A1 = porte menteaux deux ecoute
-A2 = porte menteaux trois ecoute
-A3 = porte menteaux quatre ecoute
+```
+ A0 = porte menteaux un ecoute
+ A1 = porte menteaux deux ecoute
+ A2 = porte menteaux trois ecoute
+ A3 = porte menteaux quatre ecoute
+```
+* communication Raspberry
 
-connunication rasberry
+```
 A4 = sda connection rasberry
-A5 = scl connection rasberry 
+A5 = scl connection rasberry
+```
+
+####  définir les valeurs des capteur
+
+  les variables contenant les valeur sont :
+    p1, p2, p3, p4
+  elle corresponde a chaque porte manteau.
+
+
+  pour définir les valeurs de ces variable il faut utilise l’éditeur Arduino.
+
+  1. ouvrir le fichier __code/enigme_1/enigme_1_guerande/enigme_1_guerande.ino__ avec l’éditeur Arduino.
+
+    ![open file with ide Arduino](./img_for_readme/open_file_with_ide_Arduino.png)
+
+  2. modifie la ligne ```Serial.println(analogRead(portementeau4));``` et remplace le chiffre 4 par la valeur du porte mentaux voulu.
+
+    ![highlights code on ide Arduino](./img_for_readme/change_ligne_serial.png)
+
+  3. Téléverser le code modifie sur la carte en cliquant en haut a gauche sur la flèche.
+
+    ![highlights code on ide Arduino](./img_for_readme/televerse_ide_arduino.png)
+
+  4. ouvrir le __traceur série__ en cliquant sur __outils__ puis __traceur série__.
+
+      ![place sourie on traceur serie](./img_for_readme/place_sourie_on_traceur_serie.png)
+    
--- enigme_1/enigme_1_guerande/enigme_1_guerande.ino
+++ enigme_1/enigme_1_guerande/enigme_1_guerande.ino
@@ -1,39 +1,34 @@
 #include <Wire.h>
 
 #define SLAVE_ADDRESS 0x03
+
 int number = 0;
 int state = 0;
-const int analogPin = A0;
-const int portementeau1 = A0;
-const int portementeau2 = A1;
-const int portementeau3 = A2;
-const int portementeau4 = A3;
 
-int p1[2] = {1010, 1028}; //1020 1026          // variable to store the value read
+const int analogPin = A0;
+const int portemanteau1 = A0;
+const int portemanteau2 = A1;
+const int portemanteau3 = A2;
+const int portemanteau4 = A3;
+
+int p1[2] = {1010, 1028}; //1020 1026
 int p2[2] = {565,630};
 int p3[2] = {1008,1022};
 int p4[2] = {230,350};
 bool enigme = false;
+
 void setup()
 {
-  Serial.begin(9600);              //  setup serial
+  Serial.begin(9600);
   Wire.begin(SLAVE_ADDRESS);
   Wire.onRequest(sendData);
-
 }
 
 void loop()
 {
-Serial.println(analogRead(portementeau4));
-//  Serial.print(',');
- // Serial.println( p4[0] < analogRead(portementeau4) && p4[1] >  analogRead(portementeau4));
-//  Serial.print(',');
-  Serial.println(enigme);
-//  Serial.print(',');
-//  Serial.println(analogRead(portementeau4));
-  enigme = ((p1[0] < analogRead(portementeau1) && p1[1] >  analogRead(portementeau1)) && p2[0] < analogRead(portementeau2) && p2[1] >  analogRead(portementeau2) && p3[0] < analogRead(portementeau3) && p3[1] >  analogRead(portementeau3) && p4[0] < analogRead(portementeau4) && p4[1] >  analogRead(portementeau4));
-
- delay(10);
+  Serial.println(analogRead(portemanteau4));
+  enigme = ((p1[0] < analogRead(portemanteau1) && p1[1] >  analogRead(portemanteau1)) && p2[0] < analogRead(portemanteau2) && p2[1] >  analogRead(portemanteau2) && p3[0] < analogRead(portemanteau3) && p3[1] >  analogRead(portemanteau3) && p4[0] < analogRead(portemanteau4) && p4[1] >  analogRead(portemanteau4));
+  delay(10);
 }
 
 void sendData(){
