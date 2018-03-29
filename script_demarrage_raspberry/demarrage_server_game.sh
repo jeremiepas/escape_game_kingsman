69abf6334e7a37bd7ffb91c7b64b07e360ce4d09
Jeremie <jeremie.de-pas@epitech.eu>
2018-03-30 00:33:39 +0200

    modification scripte start server

--- script_demarrage_raspberry/demarrage_server_game.sh
+++ script_demarrage_raspberry/demarrage_server_game.sh
@@ -1,11 +1,36 @@
-#!/bin/bash
+#!/bin/sh
+start()
+{
+  export LANG=C.UTF-8
+  export LC_ALL=C.UTF-8
+  export FLASK_APP=app.py
 
-export LANG=C.UTF-8
-export LC_ALL=C.UTF-8
-export FLASK_APP=app.py
+  cd /home/pi/server_escape_game/app_led
 
-cd /home/pi/server_escape_game/app_led
+  pidflask = flask run --host=0.0.0.0 &
 
-flask run --host=0.0.0.0 &
+  pidenigme = python3 gestion_enigme.py &
+}
+stop()
+{
+     # Mettez ici un kill sur votre programme
+	kill pidenigme
+  kill pidflask
+}
 
-python3 gestion_enigme.py &
+restart()
+{
+    stop;
+    sleep 1;
+    start;
+}
+case $1 in
+start)
+      start;;
+stop)
+      stop;;
+restart)
+      restart;;
+*)
+      start;;
+esac
